79dc6efb951abb55f1a0261a963d959662dbaca1
GGergo <gecsegergo006@gmail.com>
2026-02-03 10:40:18 +0100

    többi adat cuccli

--- WMSProjekt/EmployeeViewModel.cs
+++ WMSProjekt/EmployeeViewModel.cs
@@ -3,5 +3,7 @@
     public class EmployeeViewModel
     {
         public string Name { get; } = "Nagy anna";
+        public string TaxNumber { get; } = "12345678-9-10";
+        public int FreeDays { get; } = 2;
     }
 }
